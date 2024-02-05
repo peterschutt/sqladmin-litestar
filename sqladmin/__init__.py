311c33d2939ce8cb9d1ffb581303e1453c4bf49f
cemrehancavdar <cemrehancavdar@hotmail.com>
2024-02-05 17:28:25 +0300

    Update version number to 0.16.0-litestar-0.2

--- sqladmin/__init__.py
+++ sqladmin/__init__.py
@@ -1,7 +1,7 @@
 from sqladmin.application import Admin, action, expose
 from sqladmin.models import BaseView, ModelView
 
-__version__ = "0.16.0"
+__version__ = "0.16.0-litestar-0.2"
 
 __all__ = [
     "Admin",
